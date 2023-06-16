e83f804edeeec9de59bbe2e09969d8b70505b150
hongseungkeun <eiziong1@gmail.com>
2023-06-16 17:26:24 +0900

    refactor: 사용자, 사업자 공통 컬럼을 담을 userInfo 생성

---
+++ src/main/java/com/numble/reservation/domain/user/entity/UserInfo.java
@@ -0,0 +1,34 @@
+package com.numble.reservation.domain.user.entity;
+
+import com.numble.reservation.domain.user.data.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserInfo {
+    @Column(nullable = false, length = 20)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false, length = 30)
+    private String password;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role type;
+
+    @Builder
+    public UserInfo(String name, String email, String password, Role type) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+    }
+}
