2bdedcdd92a5059a9a387544bd43c1c30c85445a
hongseungkeun <eiziong1@gmail.com>
2023-06-17 00:23:16 +0900

    feat: 좌석 도메인 추가

--- src/main/java/com/numble/reservation/domain/venue/seat/domain/Seat.java
+++ src/main/java/com/numble/reservation/domain/venue/seat/domain/Seat.java
@@ -2,18 +2,36 @@ package com.numble.reservation.domain.venue.seat.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
 public class Seat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long seatId;
     @Column(nullable = false)
     private String seatNumber;
+    @Column(nullable = false)
+    @ColumnDefault("0")
     private boolean status;
+    @Column(nullable = false)
     private String seatType;
+
+    @Builder
+    public Seat(Long seatId,
+                String seatNumber,
+                boolean status,
+                String seatType) {
+        this.seatId = seatId;
+        this.seatNumber = seatNumber;
+        this.status = status;
+        this.seatType = seatType;
+    }
 }
