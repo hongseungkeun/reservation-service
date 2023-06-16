1ef16a3ec1ff63ae48f3d44c450f5c84311eb57f
hongseungkeun <eiziong1@gmail.com>
2023-06-17 00:21:50 +0900

    feat: 공연장 서비스 추가

---
+++ src/main/java/com/numble/reservation/domain/venue/service/VenueService.java
@@ -0,0 +1,33 @@
+package com.numble.reservation.domain.venue.service;
+
+import com.numble.reservation.domain.user.business.domain.BusinessUser;
+import com.numble.reservation.domain.user.service.UserService;
+import com.numble.reservation.domain.venue.domain.Venue;
+import com.numble.reservation.domain.venue.dto.request.VenueRegisterRequest;
+import com.numble.reservation.domain.venue.dto.response.VenueRegisterResponse;
+import com.numble.reservation.domain.venue.exception.VenueNotFoundException;
+import com.numble.reservation.domain.venue.repository.VenueRepository;
+import com.numble.reservation.global.exception.error.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class VenueService {
+    @Qualifier("businessUserService")
+    private final UserService userService;
+    private final VenueRepository venueRepository;
+    @Transactional
+    public VenueRegisterResponse registerVenue(VenueRegisterRequest request, BusinessUser user) {
+        Venue venue = venueRepository.save(request.toVenue(user));
+        return VenueRegisterResponse.from(venue.getVenueId());
+    }
+
+    public Venue findVenueById(Long id){
+        return venueRepository.findById(id)
+                .orElseThrow(() -> new VenueNotFoundException(ErrorCode.VENUE_NOT_FOUND));
+    }
+}
