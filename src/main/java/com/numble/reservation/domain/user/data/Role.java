f730da2bd0d9c00fab0f7cbc71cb1db7f93bf488
hongseungkeun <eiziong1@gmail.com>
2023-06-25 23:10:21 +0900

    refactor: 네이밍 변경

--- src/main/java/com/numble/reservation/domain/user/data/Role.java
+++ src/main/java/com/numble/reservation/domain/user/data/Role.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    ROLE_COMMON("사용자"),
-    ROLE_VENUE_ADMIN("공연장 관리자"),
-    ROLE_PERFORMANCE_ADMIN("공연 관리자");
+    COMMON("사용자"),
+    VENUE_ADMIN("공연장 관리자"),
+    PERFORMANCE_ADMIN("공연 관리자");
 
     private final String role;
 }
