f2b03577720838f7e2c71206486e6bdb8b907955
hongseungkeun <eiziong1@gmail.com>
2023-06-25 14:27:28 +0900

    feat: 예매 컨트롤러 추가

---
+++ src/main/java/com/numble/reservation/domain/reservation/controller/ReservationController.java
@@ -0,0 +1,26 @@
+package com.numble.reservation.domain.reservation.controller;
+
+import com.numble.reservation.domain.reservation.dto.request.ReservationCreateRequest;
+import com.numble.reservation.domain.reservation.dto.response.ReservationCreateResponse;
+import com.numble.reservation.domain.reservation.service.ReservationsService;
+import com.numble.reservation.domain.user.common.domain.CommonUser;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reservations")
+@RequiredArgsConstructor
+public class ReservationController {
+    private final ReservationsService reservationsService;
+
+    @PostMapping("/{performanceId}")
+    public ResponseEntity<ReservationCreateResponse> create(@PathVariable Long performanceId, @Valid @RequestBody ReservationCreateRequest request,
+                                                            @AuthenticationPrincipal CommonUser user){
+        ReservationCreateResponse response = reservationsService.createReservation(performanceId, request, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
