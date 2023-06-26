e342e95e34b9891059154d2c66bea70b9b4911de
hongseungkeun <eiziong1@gmail.com>
2023-06-26 17:48:11 +0900

    refactor: securityConfig 하나로 변경

--- src/main/java/com/numble/reservation/global/config/SecurityConfig.java
+++ src/main/java/com/numble/reservation/global/config/SecurityConfig.java
@@ -2,15 +2,10 @@ package com.numble.reservation.global.config;
 
 import com.numble.reservation.global.filter.JwtAuthenticationFilter;
 import com.numble.reservation.global.security.JwtTokenProvider;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,65 +14,32 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class SecurityConfig{
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @Configuration
-    @EnableWebSecurity
-    @RequiredArgsConstructor
-    public static class CommonSecurityConfig{
-        @Qualifier("commonJwtTokenProvider")
-        private final JwtTokenProvider jwtTokenProvider;
-
-        @Bean
-        public BCryptPasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
-
-        @Bean
-        protected SecurityFilterChain commonFilterChain(HttpSecurity http) throws Exception {
-            http
-                    .httpBasic(withDefaults())
-                    .csrf(AbstractHttpConfigurer::disable)
-                    .formLogin(AbstractHttpConfigurer::disable)
-                    .sessionManagement((s) -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .authorizeHttpRequests((request) -> request
-                            .requestMatchers("/reservations").hasRole("COMMON")
-                            .requestMatchers("/venues").denyAll()
-                            .requestMatchers(HttpMethod.POST,"/performances").denyAll()
-                            .anyRequest()
-                            .permitAll()
-                    )
-                    .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                            UsernamePasswordAuthenticationFilter.class);
-            return http.build();
-        }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
-    @Configuration
-    @EnableWebSecurity
-    @RequiredArgsConstructor
-    public static class BusinessSecurityConfig{
-        @Qualifier("businessJwtTokenProvider")
-        private final JwtTokenProvider jwtTokenProvider;
-
-        @Bean
-        protected SecurityFilterChain businessFilterChain(HttpSecurity http) throws Exception {
-            http
-                    .httpBasic(withDefaults())
-                    .csrf(AbstractHttpConfigurer::disable)
-                    .formLogin(AbstractHttpConfigurer::disable)
-                    .sessionManagement((s) -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .authorizeHttpRequests((request) -> request
-                            .requestMatchers("/venues").hasRole("VENUE_ADMIN")
-                            .requestMatchers(HttpMethod.POST,"/performances").hasRole("PERFORMANCE_ADMIN")
-                            .requestMatchers("/reservations").denyAll()
-                            .anyRequest()
-                            .permitAll()
-                    )
-                    .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                            UsernamePasswordAuthenticationFilter.class);
-            return http.build();
-        }
+    @Bean
+    protected SecurityFilterChain commonFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement((s) -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/reservations").hasRole("COMMON")
+                        .requestMatchers("/venues").hasRole("VENUE_ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/performances").hasRole("PERFORMANCE_ADMIN")
+                        .anyRequest()
+                        .permitAll()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
+
 }
